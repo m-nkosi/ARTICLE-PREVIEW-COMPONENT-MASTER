9993a5b467602a9832907b3f4421732aaf841cee
m-nkosi <mxolisinkosi2360@gmail.com>
2023-11-22 08:30:07 +0200

    adding the desktop version of the active state

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,8 +5,11 @@ $("button").click(()=> {
         if ($(".activeMode").is(":hidden"))
         {
             $(".activeMode").fadeIn();
+            $("button").css("background-color", "hsl(217, 19%, 35%)")
         }else {
             $(".activeMode").fadeOut();
+            $("button").css("background-color", "hsl(210, 46%, 95%");
+
         }
 })
 // making sure that if the you cle
