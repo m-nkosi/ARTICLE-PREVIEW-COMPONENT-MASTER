9e8eb54b2d39285dd296e7bbf636193c9544fecb
m-nkosi <mxolisinkosi2360@gmail.com>
2023-11-22 20:58:21 +0200

    adding the active state for mobile version

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,49 @@
 $(".activeMode").hide();
 
 // active the active mode when the button is clicked
+
 $("#activateButton").click(() => {
     // if the active tab is hidden show it
-    if ($(".activeMode").is(":hidden"))
-    {
-        $(".activeMode").fadeIn();
-        // change the background color of the button to match it
-        $("#activateButton").css("background-color", "hsl(217, 19%, 35%)");
+    if($(window).width() > 650){
+        // for desktop
+        if ($(".activeMode").is(":hidden"))
+        {
+            $(".activeMode").fadeIn();
+            // change the background color of the button to match it
+            $("#activateButton").css("background-color", "hsl(217, 19%, 35%)");
+        }else {
+            // if the active tab is  not hidden hide it
+            $(".activeMode").fadeOut();
+            // change the background color back
+            $("#activateButton").css("background-color", "hsl(210, 46%, 95%)");
+    
+        }
     }else {
-        // if the active tab is  not hidden hide it
-        $(".activeMode").fadeOut();
-        // change the background color back
-        $("#activateButton").css("background-color", "hsl(210, 46%, 95%)");
-
+        //for mobile
+        if (!$("#lower").is(":hidden")){
+            // when is not in active state
+            $("#lower").hide();
+            $(".main").addClass("activeMainMobile");
+            $(".activeMobile").css("display", "flex");
+            $(".activeMobile").append($(".share"));
+            $(".share > button").css("background-color", "hsl(210, 46%, 95%)");
+            // adding the socials
+            $(".activeMobile").prepend($(".activeMode"));
+            $(".activeMode").addClass("activeModeMobile");
+            $(".activeMode").css("height", "100%");
+            $("#arrowHead").hide();
+            $(".activeMode").fadeIn();
+        }else {
+            // when in active state
+            $("#lower").show();
+            $(".main").removeClass("activeMainMobile");
+            $(".activeMobile").css("display", "none");
+            $("#lower").append($(".share"));
+            $("#profilePicture").prepend($(".activeMode"));
+            $("#arrowHead").show();
+            $(".activeMode").removeClass("activeModeMobile");
+            $(".activeMode").hide();
+        }
     }
+   
 })
