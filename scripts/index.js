70dc29a79a31a297279b14b0f05482d481ebce9f
m-nkosi <mxolisinkosi2360@gmail.com>
2023-11-23 14:45:23 +0200

    adding the transition when a mobile version is in active mode and is transitioning to desktop

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 $(".activeMode").hide();
 
+
 // active the active mode when the button is clicked
 
 $("#activateButton").click(() => {
@@ -8,12 +9,14 @@ $("#activateButton").click(() => {
         // for desktop
         if ($(".activeMode").is(":hidden"))
         {
+            // switching on active mode
             $(".activeMode").fadeIn();
             // change the background color of the button to match it
             $("#activateButton").css("background-color", "hsl(217, 19%, 35%)");
         }else {
-            // if the active tab is  not hidden hide it
+            // switching off active mode
             $(".activeMode").fadeOut();
+            console.log("1")
             // change the background color back
             $("#activateButton").css("background-color", "hsl(210, 46%, 95%)");
     
@@ -21,7 +24,7 @@ $("#activateButton").click(() => {
     }else {
         //for mobile
         if (!$("#lower").is(":hidden")){
-            // when is not in active state
+            // when is not in active state but switching it on
             $("#lower").hide();
             $(".main").addClass("activeMainMobile");
             $(".activeMobile").css("display", "flex");
@@ -34,7 +37,7 @@ $("#activateButton").click(() => {
             $("#arrowHead").hide();
             $(".activeMode").fadeIn();
         }else {
-            // when in active state
+            // when in active state and swithing it off
             $("#lower").show();
             $(".main").removeClass("activeMainMobile");
             $(".activeMobile").css("display", "none");
@@ -46,4 +49,27 @@ $("#activateButton").click(() => {
         }
     }
    
+})
+// transition for active modes
+$(window).resize(() => {
+    // when resizing the screen
+    if ($(window).width() > 620 && ($(".activeModeMobile").length === 1))
+    {
+        // transition into desktop while in active mode
+        // first switch of the active mode
+        console.log("1");
+        $("#lower").show();
+        $(".main").removeClass("activeMainMobile");
+        $(".activeMobile").css("display", "none");
+        $("#lower").append($(".share"));
+        $(".share").append($(".activeMode"));
+        $("#arrowHead").show();
+        $(".activeMode").removeClass("activeModeMobile");
+        $(".activeMode").hide();
+        // switch it on as desktop
+        $(".activeMode").fadeIn();
+        // change the background color of the button to match it
+        $("#activateButton").css("background-color", "hsl(217, 19%, 35%)");
+
+    }
 })
