48c6753de0248464c72de9f1a5b68f7f93fd2fcc
mxolisi nkosi <mxolisinkosi2360@gmail.com>
2023-12-19 15:06:55 +0200

    making the active state of off when side loads

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
+    const $activeMode = $('.activeMode');
+    // hide active mode by default
+    $activeMode.hide();
     $('#activateButton').on('click',() => {
-        $('.activeMode').fadeToggle();
+        $activeMode.fadeToggle();
     })
     $('.activeMode').on('mouseleave', event => {
         var $share = $(event.currentTarget);
